8e693966dd78dd7d6b39ab60162a3f808c4c9513
Rabby <rabby410@gmail.com>
2023-11-11 16:30:57 +0600

    new until12

--- pages/NewDesigns.js
+++ pages/NewDesigns.js
@@ -1,12 +1,16 @@
 import Prefooter from '@/components/layout/Prefooter'
 import DesignEight from '@/components/newDesigns/DesignEight'
+import DesignEleven from '@/components/newDesigns/DesignEleven'
 import DesignFive from '@/components/newDesigns/DesignFive'
 import DesignFour from '@/components/newDesigns/DesignFour'
+import DesignNine from '@/components/newDesigns/DesignNine'
 import DesignOne from '@/components/newDesigns/DesignOne'
 import DesignSeven from '@/components/newDesigns/DesignSeven'
 import DesignSix from '@/components/newDesigns/DesignSix'
 import DesignThree from '@/components/newDesigns/DesignThree'
+import DesignTwilve from '@/components/newDesigns/DesignTwilve'
 import DesignTwo from '@/components/newDesigns/DesignTwo'
+import DewsignTen from '@/components/newDesigns/DewsignTen'
 import React from 'react'
 
 const NewDesigns = () => {
@@ -20,6 +24,10 @@ const NewDesigns = () => {
     <DesignSeven />
     <DesignSix />
     <DesignEight />
+    <DesignNine />
+    <DesignEleven />
+    <DewsignTen />
+    <DesignTwilve />
     <Prefooter/>
 
     </>
