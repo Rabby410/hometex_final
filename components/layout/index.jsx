b88140b9fed5eb845ef8ae0178a88dea3b035b39
Rabby <rabby410@gmail.com>
2023-11-22 22:54:17 +0600

    header included

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,11 +1,12 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Header2 from "./Header2";
 
 
 const Layout = ({children}) => {
     return ( 
         <div className="app__wrapper">
-            <Header key="header" />
+            <Header2 key="header" />
             {children}
             <Footer key="footer" />
         </div>
