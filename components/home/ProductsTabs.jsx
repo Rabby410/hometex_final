0b8c55d2a7d0847372bcbf884e8dedd375f5dc19
rabby home <rabby410@gmail.com>
2024-03-15 23:19:06 +0600

    aa

--- components/home/ProductsTabs.jsx
+++ components/home/ProductsTabs.jsx
@@ -14,8 +14,19 @@ import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import { FreeMode, Autoplay } from "swiper";
 import Link from "next/link";
+import ProductModal from "../common/ProductModal";
 
 const ProductsTabs = ({ products }) => {
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  // Function to open modal with product details
+  const openModal = (product) => {
+    setSelectedProduct(product);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setSelectedProduct(null);
+  };
   const params = {
     slidesPerView: 4,
     spaceBetween: 30,
@@ -87,12 +98,12 @@ const ProductsTabs = ({ products }) => {
                 {products.length > 0 &&
                   products.map((product, i) => (
                     <SwiperSlide key={i}>
-                      <Link href={`/Shop/${product.id}`}>
                         <div className="relative w-60 max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 border mt-5 object-fit">
                           <img
                             className="p-4 rounded-t-lg object-fit"
                             src={product.primary_photo}
                             alt={product.name}
+                            onClick={() => openModal(product)}
                           />
                           <div className="absolute top-0 right-0 p-2 opacity-0 hover:opacity-100 transition duration-300 ">
                             <RiShoppingBasketFill
@@ -119,17 +130,17 @@ const ProductsTabs = ({ products }) => {
                               edit={false}
                               activeColor="#ffd700"
                             />
-                            <h5 className="text-l font-semibold tracking-tight text-gray-900 dark:text-white">
+                           
+                      <Link href={`/Shop/${product.id}`}><h5 className="text-l font-semibold tracking-tight text-gray-900 dark:text-white">
                               {product.name}
                             </h5>
                             <div className="flex items-center justify-between">
                               <span className="text-xl font-bold text-red-900 dark:text-white">
                                 Price: TK {product.price}
                               </span>
-                            </div>
+                            </div></Link>
                           </div>
                         </div>
-                      </Link>
                     </SwiperSlide>
                   ))}
               </Swiper>
@@ -140,17 +151,17 @@ const ProductsTabs = ({ products }) => {
                   .filter((product) => product.category === "extra-king")
                   .map((product) => (
                     <SwiperSlide key={product.id}>
-                      <Link href={`/Shop/${product.id}`}>
                         <div className="w-full">
                           <img
                             src={product.primary_photo}
                             alt={product.name}
                             className="w-full mb-2"
+                            onClick={() => openModal(product)}
                           />
+                          <Link href={`/Shop/${product.id}`}>
                           <div className="font-bold">{product.name}</div>
-                          <div>Price: {product.price}</div>
+                          <div>Price: {product.price}</div></Link>
                         </div>
-                      </Link>
                     </SwiperSlide>
                   ))}
               </Swiper>
@@ -161,17 +172,17 @@ const ProductsTabs = ({ products }) => {
                   .filter((product) => product.category === "king")
                   .map((product) => (
                     <SwiperSlide key={product.id}>
-                      <Link href={`/Shop/${product.id}`}>
                         <div className="w-full">
                           <img
                             src={product.primary_photo}
                             alt={product.name}
                             className="w-full mb-2"
+                            onClick={() => openModal(product)}
                           />
+                          <Link href={`/Shop/${product.id}`}>
                           <div className="font-bold">{product.name}</div>
-                          <div>Price: {product.price}</div>
+                          <div>Price: {product.price}</div></Link>
                         </div>
-                      </Link>
                     </SwiperSlide>
                   ))}
               </Swiper>
@@ -182,17 +193,17 @@ const ProductsTabs = ({ products }) => {
                   .filter((product) => product.category === "semi-double")
                   .map((product) => (
                     <SwiperSlide key={product.id}>
-                      <Link href={`/Shop/${product.id}`}>
                         <div className="w-full">
                           <img
                             src={product.primary_photo}
                             alt={product.name}
                             className="w-full mb-2"
+                            onClick={() => openModal(product)}
                           />
+                          <Link href={`/Shop/${product.id}`}>
                           <div className="font-bold">{product.name}</div>
-                          <div>Price: {product.price}</div>
+                          <div>Price: {product.price}</div></Link>
                         </div>
-                      </Link>
                     </SwiperSlide>
                   ))}
               </Swiper>
@@ -203,17 +214,18 @@ const ProductsTabs = ({ products }) => {
                   .filter((product) => product.category === "single")
                   .map((product) => (
                     <SwiperSlide key={product.id}>
-                      <Link href={`/Shop/${product.id}`}>
                         <div className="w-full">
                           <img
                             src={product.primary_photo}
                             alt={product.name}
                             className="w-full mb-2"
+                            onClick={() => openModal(product)}
                           />
+                          <Link href={`/Shop/${product.id}`}>
                           <div className="font-bold">{product.name}</div>
-                          <div>Price: {product.price}</div>
+                          <div>Price: {product.price}</div></Link>
                         </div>
-                      </Link>
+                      
                     </SwiperSlide>
                   ))}
               </Swiper>
@@ -221,6 +233,7 @@ const ProductsTabs = ({ products }) => {
           </div>
         </Tabs>
       </div>
+      <ProductModal product={selectedProduct} onClose={closeModal} />
     </>
   );
 };
